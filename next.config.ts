7c38d17187ea1193b5827fbdd19e09075aba274e
krishnathakkar29 <work.krishnat@gmail.com>
2025-05-11 00:52:33 +0530

    deploy config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["s3.us-west-2.amazonaws.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
